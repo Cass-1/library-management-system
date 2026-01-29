2a662f3a3fc92216ea03a0de173d0694f4ee8607
Cass-1 <69217055+Cass-1@users.noreply.github.com>
2026-01-29 15:59:07 -0800

    feat: add handling of express server shutdown

--- server/server.ts
+++ server/server.ts
@@ -1,9 +1,22 @@
+import { debug } from "node:console";
 import { mongoDB } from "./db/conn"
 import express from 'express'
 
 const app = express()
 //TODO: need to change this so that infisical can handle it
 const PORT = 3000;
+
+const server = app.listen(PORT, () => {
+    console.log(`app listening on port ${PORT}`)
+})
+
+// on server shutdown
+process.on('SIGTERM', () => {
+    debug('SIGTERM signal received: closing HTTP server')
+    server.close(() => {
+        debug('HTTP server closed')
+    })
+})
 
 app.get("/", async (req, res) => {
 
@@ -13,6 +26,3 @@ app.get("/", async (req, res) => {
     res.send(result).status(200);
 })
 
-app.listen(PORT, () => {
-    console.log(`app listening on port ${PORT}`)
-})
