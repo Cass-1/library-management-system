a87b483b189ab0f909896fbff99a14373da15c0d
Cass-1 <69217055+Cass-1@users.noreply.github.com>
2026-01-30 16:01:23 -0800

    refactor(backend): add updated util files from branch 18-test-the-database-schema and remove old versions

---
+++ server/util/db.ts
@@ -0,0 +1,20 @@
+import { Db, MongoClient } from "mongodb";
+import { ATLAS_URI, DATABASE_NAME } from "@util/infisical.js";
+import { strict as assert } from 'node:assert';
+
+// https://www.mongodb.com/resources/languages/express-mongodb-rest-api-tutorial
+const client = new MongoClient(ATLAS_URI);
+let conn;
+try {
+    conn = await client.connect();
+} catch (e) {
+    console.error(e);
+}
+let db: Db | undefined = conn?.db(DATABASE_NAME);
+let userCol = db?.collection("users");
+
+assert(db !== undefined);
+assert(userCol !== undefined);
+
+export const mongoDB = db;
+export const userCollection = userCol;
---
+++ server/util/infisical.ts
@@ -0,0 +1,60 @@
+import { InfisicalSDK } from '@infisical/sdk';
+import { strict as assert } from 'node:assert';
+
+const MACHINE_ID = process.env.INFISICAL_MACHINE_IDENTITY_CLIENT_ID;
+const MACHINE_SECRET = process.env.INFISICAL_MACHINE_IDENTITY_CLIENT_SECRET;
+const PROJECT_ID = process.env.INFISICAL_PROJECT_ID;
+const PROJECT_ENVIRONMENT = process.env.PROJECT_ENVIRONMENT;
+
+assert(MACHINE_ID !== undefined);
+assert(MACHINE_SECRET !== undefined);
+assert(PROJECT_ID !== undefined);
+assert(PROJECT_ENVIRONMENT !== undefined);
+
+const setupClient = async () => {
+    const infisicalSdk = new InfisicalSDK();
+    await infisicalSdk.auth().universalAuth.login({
+        clientId: MACHINE_ID,
+        clientSecret: MACHINE_SECRET
+    });
+
+    return infisicalSdk;
+}
+
+let infisicalClient;
+try {
+    infisicalClient = await setupClient();
+} catch (e) {
+    console.error(e);
+}
+
+let atlas_uri, database_name, port;
+try {
+    atlas_uri = (await infisicalClient?.secrets().getSecret({
+        projectId: PROJECT_ID,
+        environment: PROJECT_ENVIRONMENT,
+        secretName: "ATLAS_URI"
+    }))?.secretValue;
+
+    database_name = (await infisicalClient?.secrets().getSecret({
+        projectId: process.env.INFISICAL_PROJECT_ID ?? "",
+        environment: process.env.PROJECT_ENVIRONMENT ?? "",
+        secretName: "DATABASE_NAME"
+    }))?.secretValue;
+
+    port = (await infisicalClient?.secrets().getSecret({
+        projectId: process.env.INFISICAL_PROJECT_ID ?? "",
+        environment: process.env.PROJECT_ENVIRONMENT ?? "",
+        secretName: "PORT"
+    }))?.secretValue;
+} catch (e) {
+    console.error(e);
+}
+
+assert(atlas_uri !== undefined);
+assert(database_name !== undefined);
+assert(port !== undefined);
+
+export const ATLAS_URI = atlas_uri;
+export const DATABASE_NAME = database_name;
+export const PORT = port;
