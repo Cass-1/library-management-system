f24fd56796c49c5471d22133d74e3b250bd2ac26
Cass-1 <69217055+Cass-1@users.noreply.github.com>
2026-02-05 15:36:56 -0800

    test(userController): improve mocking of the userService module and style changes

--- server/src/tests/userControllter.test.ts
+++ server/src/tests/userControllter.test.ts
@@ -1,18 +1,21 @@
 import * as userController from "@controllers/userController.js";
-import { vi, beforeEach, describe, expect, it } from "vitest";
+import { vi, beforeEach, describe, expect, it, Mock } from "vitest";
 import { expressResponseMock } from "./util/expressResponseMock.js";
-import * as UserService from "@services/userService.js"
-import { User } from "@/models/User.js";
 
-// vi.mock("../services/userService.ts", async () => {
-//     const originalModule = await vi.importActual("../services/userService.ts");
+// https://github.com/vitest-dev/vitest/discussions/3589
+const serviceMocks = vi.hoisted(() => {
+    return {
+        getUser: vi.fn(),
+        deleteUser: vi.fn()
+    }
+});
 
-//     return {
-//         ...originalModule,
-//         getUser: async () => vi.fn(),
-//         deleteUser: async () => vi.fn()
-//     }
-// });
+vi.mock("../services/userService.js", () => {
+    return {
+        getUser: serviceMocks.getUser,
+        deleteUser: serviceMocks.deleteUser
+    }
+})
 
 describe("User Controller Unit Tests", () => {
     beforeEach(() => {
@@ -20,24 +23,22 @@ describe("User Controller Unit Tests", () => {
     });
 
     it("getUser should get a user", async () => {
-        const spy = vi.spyOn(UserService, "getUser").mockResolvedValue({ test: "hello" } as any as User)
         const mockReq = { params: { id: "1" } };
+        (serviceMocks.getUser as Mock).mockResolvedValue({ test: "hello" });
 
         await userController.getUser(mockReq as any, expressResponseMock);
 
-        expect(spy).toHaveBeenCalled();
         expect(expressResponseMock.status).toHaveBeenCalledWith(200);
-        expect(expressResponseMock.json).toHaveBeenCalledWith({ test: "hello" } as any as User);
+        expect(expressResponseMock.json).toHaveBeenCalledWith({ test: "hello" });
     });
 
     it("deleteUser should delete user", async () => {
-        const spy = vi.spyOn(UserService, "deleteUser").mockResolvedValue({ test: "hello" } as any);
         const mockReq = { params: { id: "1" } };
+        (serviceMocks.deleteUser as Mock).mockResolvedValue({ test: "hello" });
 
         await userController.deleteUser(mockReq as any, expressResponseMock);
 
-        expect(spy).toHaveBeenCalled();
         expect(expressResponseMock.status).toHaveBeenCalledWith(200);
-        expect(expressResponseMock.json).toHaveBeenCalledWith({ test: "hello" } as any);
+        expect(expressResponseMock.json).toHaveBeenCalledWith({ test: "hello" });
     })
 })
